39e4fe7bcff84166e25dd1724f1dc981459aa666
YeiyoNathnael <138720671+YeiyoNathnael@users.noreply.github.com>
2025-12-03 03:23:52 +0400

    Create a simple generic stack using linkedlist

--- chapter4/stacks/GenericStack.java
+++ chapter4/stacks/GenericStack.java
@@ -1,4 +1,39 @@
-public class GenericStack {
-    
+import java.util.*;
+public class GenericStack<E> {
+
+/*Operations: 
+
+- Push() : adds to the top of the stack.✓ 
+- Pop() : takes the top element off the stack.✓
+- getSize() .✓
+- IsEmpty : checks if the stack is empty.✓
+- Top : returns a copy of the top element.✓
+
+*/
+
+	LinkedList<E> list = new LinkedList<>();
+	
+	public boolean IsEmpty(){
+		return getSize() == 0;
+	}
+	public int getSize(){
+		return list.size();
+	}
+	public void push( E element){
+		list.add(element);
+	}
+	public E pop(){
+		E popped = list.get(getSize() - 1);	
+		list.remove(list.get(getSize() - 1);
+		return popped;
+	}
+	public E peek(){
+		E Topped = list.get(getSize() - 1);	
+		return popped;
+	}
+	
+	
+		
+
 }
 
--- chapter4/stacks/StackAlgorithms.java
+++ chapter4/stacks/StackAlgorithms.java
@@ -1,4 +1,21 @@
 public class StackAlgorithms {
     
+	public static void main(String[] args) {
+    		//
+	}
+
+	//Algorithm5.2: Write a method that receive two stacks’ objects S1 and S2.
+	// The method returns true if the two stacks are identical, false otherwise. 
+	//The two stacks should be unchanged when the method terminates.
+	public void Algorithm1() {
+	
+	}
+
+	//Algorithm5.6: Given a stack of elements S1,
+	// you are requested to create an exact copy of S1 leaving S1 unchanged.
+	// Use only the stack predefined methods and an additional data structure object
+	public void Algorithm2() {
+	
+	}
 }
 
