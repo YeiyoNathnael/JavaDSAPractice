73f6730e521bf67722b9dfd098e96e74fc356a84
YeiyoNathnael <138720671+YeiyoNathnael@users.noreply.github.com>
2025-12-03 04:40:11 +0400

    Complete the StackAlgorithms and WellFormedSets

--- chapter4/stacks/StackAlgorithms.java
+++ chapter4/stacks/StackAlgorithms.java
@@ -1,21 +1,56 @@
 public class StackAlgorithms {
     
 	public static void main(String[] args) {
-    		//
+    		//Tests
+		//ToDo
 	}
 
 	//Algorithm5.2: Write a method that receive two stacks’ objects S1 and S2.
 	// The method returns true if the two stacks are identical, false otherwise. 
 	//The two stacks should be unchanged when the method terminates.
-	public void Algorithm1() {
-	
+	public boolean Algorithm1(Stack<E> s1,Stack<E> s2) {
+		Stack<E> s1_cpy = new Stack<>();
+		Stack<E> s2_cpy = new Stack<>();
+		boolean identical = true;
+		
+		while(!s1.isEmpty()){
+			s1_cpy.push(s1.pop());
+		}
+		while(!s2.isEmpty()){
+			s2_cpy.push(s2.pop());
+		}
+
+		while(!s1_cpy.isEmpty() && !s2_isEmpty()){
+			E temp1 = s1_cpy.pop();
+			E temp2 = s2_cpy.pop();
+		
+			if(temp1.equals(temp2)) identical = false;
+			s1.push(temp1);
+			s2.push(temp2);
+		}
+		while(!s1_cpy.isEmpty()) s1.push(s1_cpy.pop());
+		while(!s2_cpy.isEmpty()) s2.push(s2_cpy.pop());
+
+		return identical && s1.size() == s2.size();
+		
+		
 	}
 
 	//Algorithm5.6: Given a stack of elements S1,
 	// you are requested to create an exact copy of S1 leaving S1 unchanged.
 	// Use only the stack predefined methods and an additional data structure object
-	public void Algorithm2() {
-	
+	public Stack<E> Algorithm2(Stack<E> s) {
+		ArrayList<E> list = new ArrayList<>();
+		Stack<E> s_cpy = new Stack<>();
+
+		while(!s.isEmpty()){
+			list.add(s.pop());
+		}
+		for(E item:list){
+			s.push(item);
+			s_cpy.push(item);
+		}
+		return s_cpy;
 	}
 }
 
--- chapter4/stacks/WellFormedSets.java
+++ chapter4/stacks/WellFormedSets.java
@@ -1,8 +1,55 @@
+import java.util.*;
 public class WellFormedSets{
-	
-	public static void main(Strings[]args){
-	
 
+	/*
+Problem: determine whether a set of parentheses is “well formed”. 
+Here, we will consider parenthesis pairs: ( ), [ ], { } 
+
+◼ Examples:
+◼ well-formed expressions
+	◼ (xx(xx( ) )xx)
+	◼ [ ] ( ) { }
+	◼ ( { } { xxx} xxx ( ) xxx)
+	◼ ( [ { [ ( ( [ {x} ] ) x ) ] }x ] )
+◼ Ill-formed expressions
+	◼ (xx(xx( ))xxx)xxx)
+	◼ ] [
+	◼ (xx[xxx)xx]
+	
+	*/
+
+	
+	public static void main(String[] args){
+				
+		String expression = "( [ { [ ( ( [ {x} ] ) x ) ] }x ] )";
+		if(wellFormed(expression)) System.out.println("YES");
+	}
+
+	public static boolean wellFormed(String expression){
+		
+		Stack<Character> set = new Stack<>();
+		
+		for(int i = 0; i < expression.length(); i++){
+			set.push(expression.charAt(i));
+		}
+
+		int full = 0;
+		while(!set.isEmpty() && full>= 0){
+			
+			char current = set.pop();
+			if(isOpen(current)) full--;
+			if(isClosed(current))full++;
+		}
+		return full == 0;
+		
+		
+	}
+	
+	public static boolean isOpen(char c ){
+		return c == '{'|| c == '('|| c == '[';
+	}
+	public static boolean isClosed(char c ){
+		return c == '}'|| c == ')'|| c == ']';
 	}
 
 }
